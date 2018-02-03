6c8b3e0dd76c6ef0ca866055d46ebe8c18e5d420
Blazelisa <blazelisa@gmail.com>
2018-02-03 11:26:38 +0800

    change

---
+++ app/src/main/java/tw/com/blazelisa/benqio_class_four/MainActivity.java
@@ -0,0 +1,13 @@
+package tw.com.blazelisa.benqio_class_four;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
